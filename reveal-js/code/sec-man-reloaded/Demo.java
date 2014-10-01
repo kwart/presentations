ce907518874b1fd3d5731441d2cd731800dac882
Josef Cacek <josef.cacek@gmail.com>
2014-10-02 00:19:30 +0200

    1st demo in one step

--- reveal-js/code/sec-man-reloaded/Demo.java
+++ reveal-js/code/sec-man-reloaded/Demo.java
@@ -1,6 +1,12 @@
+/**
+ * Demo class, which prints values of system properties.
+ * Names of the properties are provided as program arguments.
+ */
 public class Demo {
   
   public static void main(String args[]) {
-    // TODO
+    for (String arg:args) {
+      System.out.println(arg + " = " + System.getProperty(arg));
+    }
   }
 }
