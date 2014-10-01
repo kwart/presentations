52ddb51358c87ece32e56ab8327a4b4b9dd35a64
Josef Cacek <josef.cacek@gmail.com>
2014-10-01 09:02:53 +0200

    demo update

--- reveal-js/code/sec-man-reloaded/Demo.java
+++ reveal-js/code/sec-man-reloaded/Demo.java
@@ -1,8 +1,6 @@
 public class Demo {
   
   public static void main(String args[]) {
-    for (String arg:args) {
-      System.out.println(arg + " = " + System.getProperty(arg));
-    }
+    // TODO
   }
 }
